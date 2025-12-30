ddede48f237d8340657f2b8f24b9d31fb895d85e
Iypan Amdin <manieee1126@gmail.com>
2025-12-30 11:05:11 +0530

    producton refresh issue

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,6 @@ import Coaches from "./pages/Coaches";
 import Gallery from "./pages/Gallery";
 import Events from "./pages/Events";
 import Contact from "./pages/Contact";
-import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
 
@@ -28,7 +27,7 @@ const App = () => (
           <Route path="/gallery" element={<Gallery />} />
           <Route path="/events" element={<Events />} />
           <Route path="/contact" element={<Contact />} />
-          <Route path="*" element={<NotFound />} />
+          <Route path="*" element={<Index />} />
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
